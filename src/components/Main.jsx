d1120cefb7b8dec1f365bf65c3d514f58539bfb6
Kristin Reddington <kristinreddington@gmail.com>
2024-08-17 22:45:56 -0400

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+export default function Main(props) {
+    const { data } = props
+    return (
+        <div className="imgContainer">
+            <img className="bgImage" src={data.hdurl} alt={data.title || 'bg-img'} />
+        </div>
+    )
+}
